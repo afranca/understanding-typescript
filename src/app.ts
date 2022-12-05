cbef86292cba3024962970b23be53cf022257695
Alexandre Franca <Alexandre.Franca@bjss.com>
2022-12-05 08:42:36 +0000

    refact: extacting list item as a new ProjecItem Component class

--- src/app.ts
+++ src/app.ts
@@ -150,6 +150,31 @@ abstract class Component<T extends HTMLElement, U extends HTMLElement> {
 
 }
 
+// Project Item class
+class ProjecItem extends Component<HTMLUListElement, HTMLLIElement>{
+    private project: Project;   
+
+    constructor(
+        hostId: string,
+        project: Project
+        )   {
+        super('single-project', hostId, false, project.id);       
+        this.project = project;
+
+        this.configure();
+        this.renderContent();
+    }
+
+    configure(): void {
+        //throw new Error("Method not implemented.");
+    }
+    renderContent(): void {
+        this.element.querySelector('h2')!.textContent = this.project.title;
+        this.element.querySelector('h3')!.textContent = this.project.numberOfPeople.toString();
+        this.element.querySelector('p')!.textContent = this.project.description;
+    }
+}
+
 // Project List class
 class ProjectList extends Component<HTMLDivElement, HTMLElement> {
 
@@ -191,9 +216,7 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement> {
         listEl.innerHTML = '';
 
         for(const prjItem of this.assignedProjects){
-            const listItem = document.createElement('li');
-            listItem.textContent = prjItem.title;
-            listEl.appendChild(listItem);
+            new ProjecItem(this.element.querySelector('ul')!.id, prjItem);
         }
     }
 }
