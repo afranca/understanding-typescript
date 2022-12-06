5775d63d9d6eeeb8703afe4893fe2ad31fc08944
Alexandre Franca <Alexandre.Franca@bjss.com>
2022-12-06 09:29:42 +0000

    feat: creating GETTER for persons assigned

--- src/app.ts
+++ src/app.ts
@@ -154,6 +154,13 @@ abstract class Component<T extends HTMLElement, U extends HTMLElement> {
 class ProjecItem extends Component<HTMLUListElement, HTMLLIElement>{
     private project: Project;   
 
+    get persons(){
+        if (this.project.numberOfPeople === 1){
+            return '1 Person';
+        }
+        return `${this.project.numberOfPeople} People`;
+    }
+
     constructor(
         hostId: string,
         project: Project
@@ -170,7 +177,7 @@ class ProjecItem extends Component<HTMLUListElement, HTMLLIElement>{
     }
     renderContent(): void {
         this.element.querySelector('h2')!.textContent = this.project.title;
-        this.element.querySelector('h3')!.textContent = this.project.numberOfPeople.toString();
+        this.element.querySelector('h3')!.textContent = this.persons + ' assigned';
         this.element.querySelector('p')!.textContent = this.project.description;
     }
 }
